164cc976c8136b375b3d7ff05cbe051827ee935b
giuliano.lautaro <gigi2001.es@gmail.com>
2023-12-23 10:33:01 -0300

    navigation

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
+<<<<<<< HEAD
 import { ActivityIndicator } from 'react-native';
 import CategoriesScreen from './src/screens/screens/CategoriesScreen'
 import ProductsByCategoryScreen from './src/screens/screens/ProductsByCategoryScreen'
 import {useFonts} from 'expo-font'
 
 export default function App() {
+=======
+import { ActivityIndicator, Text } from 'react-native';
+//import CategoriesScreen from './src/screens/CategoriesScreen'
+//import ProductDetailScreen from './src/screens/ProductDetailScreen';
+//import ProductsByCategoryScreen from './src/screens/ProductsByCategoryScreen'
+import {useFonts} from 'expo-font'
+import {useState} from 'react';
+import Navigator from './src/navigation/Navigator'
+
+export default function App() {
+  const [categorySelected, setCategorySelected] = useState('')
+  const [productIdSelected, setProductIdSelected] = useState(null)
+
+  console.log("categoria sleccionada:", categorySelected)
+  
+>>>>>>> b8b1966 (navigation)
      
   const [fontLoaded] = useFonts({
     'Alegreya-regular' : require('./assets/fonts/Alegreya-Regular.ttf'),
@@ -12,9 +29,32 @@ export default function App() {
 
  if(!fontLoaded) return <ActivityIndicator />
 
+<<<<<<< HEAD
 
   return (
     <ProductsByCategoryScreen /> 
   );
+=======
+ const onSelectCategory = (category)=> {
+  setCategorySelected(category)
+ }
+
+ const onSelectProductId = (productId)=> {
+  setProductIdSelected(productId)
+}
+
+  return (
+    <>
+    <Navigator/>
+    {/* {
+      categorySelected
+      ?
+      <ProductsByCategoryScreen category={categorySelected} /> 
+      :
+      <CategoriesScreen onSelectCategoryEvent={onSelectCategory} /> 
+        } */}
+      </>
+ );
+>>>>>>> b8b1966 (navigation)
 }
 
---
+++ src/screens/CategoriesScreen.jsx
@@ -0,0 +1,29 @@
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import Header from '../components/Header'
+import categories_data from  '../data/categories_data.json'
+import CategoryItem from '../components/CategoryItem'
+
+const CategoriesScreen = ({navigation}) => {
+  
+  const renderCategoryItem = ({item}) => (
+    <CategoryItem category={item} navigation={navigation}/>
+  )
+
+  
+
+  return(
+    <>
+    <Header title="Categories" />
+    <FlatList
+         data={categories_data}
+         renderItem={renderCategoryItem}
+         keyExtractor={item=>item}
+    />
+    </>
+  )
+}
+
+export default CategoriesScreen
+
+const styles = StyleSheet.create({
+})
